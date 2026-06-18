6fd6ea38aa5449bbfe0c6c601aa7810f579cc63f
최현지 <choihyunji@choehyeonjiui-MacBookPro.local>
2026-06-18 09:56:28 +0900

    fix github pages

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+
+const path = require('path');
 
 const nextConfig = {
+  output: 'export',
+
+  basePath: '/book-microsite',
+  assetPrefix: '/book-microsite/',
+
+  images: {
+    unoptimized: true,
+  },
+
   reactStrictMode: true,
+
   experimental: {
     turbo: {
       root: path.resolve(__dirname),
     },
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
